2d67566519da395001dfd44cd63a9c592c083445
Alejandro <alex.garcia.2002@hotmail.com>
2023-10-26 09:52:42 +0200

    index.html creado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Gola") 
+import { Categoria } from "./Categoria.js";
+
+document.addEventListener("DOMContentLoaded", function() {
+    const categoryTable = document.getElementById("category-table");
+
+    categoryTable.addEventListener("click", function(event) {
+        if (event.target.tagName === "A") {
+          
+            const categoryName = event.target.textContent;            
+            const category = new Categoria(categoryName);            
+            localStorage.setItem("categoria", JSON.stringify(category));            
+            window.location.href = `frases.html`;            
+        }
+    });
+
+    
+    fetch("https://api.chucknorris.io/jokes/categories")
+        .then(response => response.json())
+        .then(data => {
+            
+            data.forEach(category => {
+                const row = categoryTable.insertRow();
+                const cell = row.insertCell(0);                
+                const categoryLink = document.createElement("a");
+                categoryLink.href = "#"; 
+                categoryLink.textContent = category;
+                cell.appendChild(categoryLink);
+            });
+        })
+        .catch(error => {
+            console.error("Error al obtener categorías: " + error);
+        });
+});
